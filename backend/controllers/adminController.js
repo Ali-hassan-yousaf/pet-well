edfa4efcce01c5f40fc5c63b6820cdedf6286b26
Ali-hassan-yousaf <alihassan.tech27@gmail.com>
2025-05-07 13:34:34 +0500

    Update adminController.js

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -269,7 +269,7 @@ const addD = async (req, res) => {
         });
 
         await newD.save();
-        res.json({ success: true, message: "Saloon added successfully!" });
+        res.json({ success: true, message: "added successfully!" });
     } catch (error) {
      res.json({ success: false, message: error.message });
 
